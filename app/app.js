19c021f73283ae0e655f969ddd54e6e9b3dfa4da
Cem Staveley <c115860@1158960.(none)>
2013-11-02 17:34:10 +0000

    removed artefacts

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('randomMovie', ['randomMovieController', 'randomMovieService', 'randomMovieDirective']);
+var app = angular.module('randomMovie', ['randomMovieController', 'randomMovieService']);
 
 app.config( function ($httpProvider) {
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
