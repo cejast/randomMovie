7a08fc1835251d4e0b07c0ebc9572dbd75a0d8e3
Cem Staveley <c115860@1158960.(none)>
2013-11-03 14:14:42 +0000

    ui cleanup

--- app/controller.js
+++ app/controller.js
@@ -1,45 +1,53 @@
 var randomMovieController = angular.module('randomMovieController', ['randomMovieService']);
 
-randomMovieController.controller('RandomMovie', function($scope, $http, service){	
+randomMovieController.controller('RandomMovie', function($scope, $http, api, apiUrl, genres){	
 
 	$scope.initialize = function(){
-				var api = service.getAPI();
 
-				$http({method: 'GET', url: 'http://api.themoviedb.org/3/genre/list?api_key=' + api})
+				$http({method: 'GET', url: apiUrl + 'genre/list?api_key=' + api})
 				.success(function(data){
-					service.setGenres(data);
+					genres.setGenres(data);
 				})
 				.error(function(){
 					console.log('ERROR: unable to GET (initialize)');
 				});
 
-				$http({method: 'GET', url: 'http://api.themoviedb.org/3/configuration?api_key=' + api})
+				$http({method: 'GET', url: apiUrl + 'configuration?api_key=' + api})
 				.success(function(data){
 					$scope.configuration = data;
-					$scope.background = $scope.configuration.images.base_url + 'original' + $scope.movieResult.backdrop_path;
+					$scope.background = $scope.configuration.images.base_url + 'w1280' + $scope.movieResult.backdrop_path;
 				})
 				.error(function(){
 					console.log('ERROR: unable to GET (configuration)')
 				});
 	}
 
-	$scope.genres = service.getGenres(); //list of all genres
+	$scope.genres = genres.getGenres(); //list of all genres
 	$scope.selectedGenres = []; //user selected genres
 	$scope.random = 0; //random genre selected from selectedGenres[]
-	$scope.movieResult = {"adult":false,"backdrop_path":"/jjAq3tCezdlQduusgtMhpY2XzW0.jpg","belongs_to_collection":{"id":121938,"name":"The Hobbit Collection","poster_path":"/4MyjzLpdX6H0Voj7H2kIgNgowli.jpg","backdrop_path":"/7wO7MSnP5UcwR2cTHdJFF1vP4Ie.jpg"},"budget":250000000,"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":14,"name":"Fantasy"}],"homepage":"http://www.thehobbit.com/","id":49051,"imdb_id":"tt0903624","original_title":"The Hobbit: An Unexpected Journey","overview":"Bilbo Baggins, a hobbit enjoying his quiet life, is swept into an epic quest by Gandalf the Grey and thirteen dwarves who seek to reclaim their mountain home from Smaug, the dragon.","popularity":35.0927029899782,"poster_path":"/nGLrjWHsFcz62Xw6Epz84j5faWZ.jpg","production_companies":[{"name":"Warner Bros. Pictures","id":174},{"name":"Metro-Goldwyn-Mayer Pictures","id":6127}],"production_countries":[{"iso_3166_1":"US","name":"United States of America"},{"iso_3166_1":"NZ","name":"New Zealand"}],"release_date":"2012-12-12","revenue":1017003568,"runtime":169,"spoken_languages":[{"iso_639_1":"en","name":"English"}],"status":"Released","tagline":"From the smallest beginnings come the greatest legends.","title":"The Hobbit: An Unexpected Journey","vote_average":6.5,"vote_count":3867};
-	$scope.date = $scope.movieResult.release_date.split("-", 1);
 
-	// $scope.possibleResults = function(){
-	// 	var result = 0;
-	// 	for(var i = 0; i <= $scope.getSearchGenres.length - 1; i++){
-	// 		result += $scope.getSearchGenres[i].results;
-	// 	}
-	// 	return result;
-	// }
+
+	//movie information
+	$scope.movieResult = {"adult":false,"backdrop_path":"/jjAq3tCezdlQduusgtMhpY2XzW0.jpg","belongs_to_collection":{"id":121938,"name":"The Hobbit Collection","poster_path":"/4MyjzLpdX6H0Voj7H2kIgNgowli.jpg","backdrop_path":"/7wO7MSnP5UcwR2cTHdJFF1vP4Ie.jpg"},"budget":250000000,"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":14,"name":"Fantasy"}],"homepage":"http://www.thehobbit.com/","id":49051,"imdb_id":"tt0903624","original_title":"The Hobbit: An Unexpected Journey","overview":"Bilbo Baggins, a hobbit enjoying his quiet life, is swept into an epic quest by Gandalf the Grey and thirteen dwarves who seek to reclaim their mountain home from Smaug, the dragon.","popularity":35.0927029899782,"poster_path":"/nGLrjWHsFcz62Xw6Epz84j5faWZ.jpg","production_companies":[{"name":"Warner Bros. Pictures","id":174},{"name":"Metro-Goldwyn-Mayer Pictures","id":6127}],"production_countries":[{"iso_3166_1":"US","name":"United States of America"},{"iso_3166_1":"NZ","name":"New Zealand"}],"release_date":"2012-12-12","revenue":1017003568,"runtime":169,"spoken_languages":[{"iso_639_1":"en","name":"English"}],"status":"Released","tagline":"From the smallest beginnings come the greatest legends.","title":"The Hobbit: An Unexpected Journey","vote_average":6.5,"vote_count":3867};
+	$scope.title = function(){
+		return $scope.movieResult.title;
+	}
+	$scope.date = $scope.movieResult.release_date.split("-", 1);
+	$scope.tagline = function(){
+		if($scope.movieResult.tagline === ""){
+			return null;
+		}
+		return '"' + $scope.movieResult.tagline + '"';
+	}
+	$scope.poster = function(){
+		return $scope.configuration.images.base_url + 'w185' + $scope.movieResult.poster_path;
+	}
+	$scope.overview = function(){
+		return $scope.movieResult.overview;
+	}
 
 	$scope.searchMovie = function(id, genre){
-		var api = service.getAPI();
-		$http({method: 'GET', url: 'http://api.themoviedb.org/3/genre/' + id + '/movies?api_key=' + api})
+		$http({method: 'GET', url: apiUrl + 'genre/' + id + '/movies?api_key=' + api})
 		.success(function(data){
 			var movieNumber = Math.floor((Math.random() * data.total_results) + 1);
 			var page = Math.floor(movieNumber / 20);
@@ -54,10 +62,9 @@ randomMovieController.controller('RandomMovie', function($scope, $http, service)
 	}
 
 	$scope.getMovie = function(id, page, item){
-		var api = service.getAPI();
-		$http({method: 'GET', url: 'http://api.themoviedb.org/3/genre/' + id + '/movies?api_key=' + api + '&page=' + page})
+		$http({method: 'GET', url: apiUrl + 'genre/' + id + '/movies?api_key=' + api + '&page=' + page})
 		.success(function(data){
-			$http({method:'GET', url:'http://api.themoviedb.org/3/movie/' + data.results[item].id + '?api_key=' + api})
+			$http({method:'GET', url:apiUrl + 'movie/' + data.results[item].id + '?api_key=' + api})
 			.success(function(data){
 				$scope.movieResult = data;
 				$scope.background = $scope.configuration.images.base_url + 'original' + data.backdrop_path;				
@@ -87,14 +94,22 @@ randomMovieController.controller('RandomMovie', function($scope, $http, service)
 		return 0;
   	}
 
+  	$scope.getIndex = function (array, id){
+  		for(var i = 0; i <= array.length - 1; i++){
+  			if(array[i].id === id)
+  				return i;
+  		}
+  		return null;
+  	}
+
 	$scope.moveGenre = function(id, genre){
 		if($scope.hasItem($scope.selectedGenres, id)){
-			$scope.deleteItem($scope.selectedGenres, id);
-			$scope.genres.push({id: id, genre: genre});			
+			$scope.genres[$scope.getIndex($scope.genres, id)].class = genres.setClass(false);
+			$scope.deleteItem($scope.selectedGenres, id);			
 		}
 		else{
-			$scope.selectedGenres.push({id: id, genre: genre});
-			$scope.deleteItem($scope.genres, id);
+			$scope.genres[$scope.getIndex($scope.genres, id)].class = genres.setClass(true);
+			$scope.selectedGenres.push({id: id, genre: genre, class: genres.setClass(true)});
 		}
 	}
 
@@ -102,6 +117,15 @@ randomMovieController.controller('RandomMovie', function($scope, $http, service)
 		var random = Math.floor((Math.random() * $scope.selectedGenres.length) + 0)
 		$scope.random = $scope.selectedGenres[random];
 		$scope.searchMovie($scope.random.id, $scope.random.genre);
+	}
+
+	$scope.movieResultGenresClass = function(genre){
+		for(var i = 0; i <= $scope.selectedGenres.length - 1; i++){
+			if($scope.selectedGenres[i].genre === genre){
+				return 'selectedGenre btn btn-info';
+			}
+		}
+		return 'genreSelect btn btn-primary';
 	}
 
 	$scope.getBackground = function(){
--- app/service.js
+++ app/service.js
@@ -1,18 +1,18 @@
 var randomMovieService = angular.module('randomMovieService', []);
 
-randomMovieService.service('service', function($http){
-
+randomMovieService.factory('api', function(){
 	var api = '09e09aca22efd2b0335a2b5f780baad8';
+	return api;
+})
+
+randomMovieService.factory('apiUrl', function(){
+	var apiUrl = 'http://api.themoviedb.org/3/';
+	return apiUrl;
+})
+
+randomMovieService.service('genres', function(){
 	var genres = [];
 	var blacklist = ['Erotic', 'Film Noir', 'Fan Film', 'Neo Noir', 'Road Movie', 'Sport', 'Sporting Event', 'TV movie'];
-
-	this.getAPI = function(){
-		return api;
-	}
-
-	this.getGenres = function(){
-		return genres;
-	}
 
 	this.blacklistHas = function(name){
 		for (var i = 0; i <= blacklist.length - 1; i++){
@@ -21,22 +21,27 @@ randomMovieService.service('service', function($http){
 		return false;
 	}
 
+	this.getGenres = function(){
+		return genres;
+	}
+
 	this.setGenres = function(data){
 		for(var i = 0; i <= data.genres.length - 1; i++){
 			if(!this.blacklistHas(data.genres[i].name)){
 				genres.push({
 					id: data.genres[i].id,
-					genre: data.genres[i].name
+					genre: data.genres[i].name,
+					class: this.setClass(false),
 				});
 			}
 		}
-	};
+	}
 
-	this.getGenreById = function(id){
-		for(var i = 0; i <= genres.length - 1; i++)
-			if(genres[i].id === id){
-				return genres[i].genre;
-			}
+	this.setClass = function(boolean){
+		if(boolean){
+			return "selectedGenre btn btn-info";
+		}
+		return "genreSelect btn btn-primary";
 	}
 
 });
